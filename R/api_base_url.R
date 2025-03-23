558f9f6aebbca28462bf9346e4a27e246292e8f8
Christos Avrilionis <avrilionis@gmail.com>
2025-03-23 22:19:29 +0100

    Improved documentation

--- R/api_base_url.R
+++ R/api_base_url.R
@@ -1,10 +1,20 @@
-#' Get the API base URL
+#' Retrieve the Base URL for the data.public.lu API
 #'
-#' @returns A character vector
+#' This function returns the base URL used to access the data.public.lu open data API.
+#' It provides a consistent and easily accessible way to construct API requests.
+#'
+#' @return A character string representing the base URL of the data.public.lu API.
 #' @export
 #'
 #' @examples
-#' api <- api_base_url()
+#' # Obtain the base URL for the API.
+#' base_url <- api_base_url()
+#' print(base_url)
+#'
+#' # Construct a full API request for a specific endpoint (example).
+#' endpoint <- "datasets"
+#' full_url <- paste0(api_base_url(), "/", endpoint)
+#' print(full_url)
 api_base_url <- function() {
   return("https://data.public.lu/api/1")
 }
